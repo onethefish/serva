b50efed3c5b6737a0dbc07b78df20f884c2f5dca
onethefish <95610948+onethefish@users.noreply.github.com>
2026-05-06 10:33:09 +0800

    需求：多余的注解

--- serva-starter-file/src/main/java/cn/fish/cloud/serva/file/operate/config/ServaFileConfig.java
+++ serva-starter-file/src/main/java/cn/fish/cloud/serva/file/operate/config/ServaFileConfig.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Data
 @ToString
